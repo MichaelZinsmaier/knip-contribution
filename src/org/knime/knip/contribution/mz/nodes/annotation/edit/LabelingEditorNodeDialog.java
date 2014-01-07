1de9c6bf522a17f0b80e12b9ffd4056f0f3c5582
MichaelZinsmaier <michael.zinsmaier@googlemail.com>
2014-01-07 23:03:56 +0100

    licensing stuff

--- src/org/knime/knip/contribution/mz/nodes/annotation/edit/LabelingEditorNodeDialog.java
+++ src/org/knime/knip/contribution/mz/nodes/annotation/edit/LabelingEditorNodeDialog.java
@@ -1,3 +1,30 @@
+/*Copyright (C) 2014 Michael Zinsmaier
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+*/
 package org.knime.knip.contribution.mz.nodes.annotation.edit;
 
 import javax.swing.event.ChangeEvent;
@@ -33,10 +60,10 @@ import org.knime.knip.io.nodes.annotation.DialogComponentAnnotatorView;
 /**
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael Zinsmaier</a>
  */
-public class LabelEditorNodeDialog<T extends RealType<T> & NativeType<T>, L extends Comparable<L>> extends
+public class LabelingEditorNodeDialog<T extends RealType<T> & NativeType<T>, L extends Comparable<L>> extends
 		DataAwareDefaultNodeSettingsPane {
 
-	private static final NodeLogger LOGGER = NodeLogger.getLogger(LabelEditorNodeDialog.class);
+	private static final NodeLogger LOGGER = NodeLogger.getLogger(LabelingEditorNodeDialog.class);
 	
 	private static final String APPEND_DEFAULT = "_Label";
 
@@ -47,14 +74,14 @@ public class LabelEditorNodeDialog<T extends RealType<T> & NativeType<T>, L exte
 			.createColSuffixNodeModel();
 
 	@SuppressWarnings("unchecked")
-	public LabelEditorNodeDialog() {
+	public LabelingEditorNodeDialog() {
 		super();
 
 		removeTab("Options");
 		createNewTab("Selection");
 		createNewGroup("Image Annotation");
 
-		SettingsModelLabelAnnotator annotatorSM = LabelEditorNodeModel
+		SettingsModelLabelAnnotator annotatorSM = LabelingEditorNodeModel
 				.createAnnotatorSM();
 		m_dialogComponentAnnotator = new DialogComponentAnnotatorView<Labeling<String>>(
 				new LabelAnnotatorView(), annotatorSM);
--- src/org/knime/knip/contribution/mz/nodes/annotation/edit/LabelingEditorNodeFactory.java
+++ src/org/knime/knip/contribution/mz/nodes/annotation/edit/LabelingEditorNodeFactory.java
@@ -1,3 +1,30 @@
+/*Copyright (C) 2014 Michael Zinsmaier
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+*/
 package org.knime.knip.contribution.mz.nodes.annotation.edit;
 
 import net.imglib2.type.NativeType;
@@ -11,15 +38,15 @@ import org.knime.knip.base.nodes.view.TableCellViewNodeView;
 /**
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael Zinsmaier</a>
  */
-public class LabelEditorNodeFactory<T extends RealType<T> & NativeType<T>, L extends Comparable<L>>
-		extends NodeFactory<LabelEditorNodeModel<T>> {
+public class LabelingEditorNodeFactory<T extends RealType<T> & NativeType<T>, L extends Comparable<L>>
+		extends NodeFactory<LabelingEditorNodeModel<T>> {
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public LabelEditorNodeModel<T> createNodeModel() {
-		return new LabelEditorNodeModel<T>();
+	public LabelingEditorNodeModel<T> createNodeModel() {
+		return new LabelingEditorNodeModel<T>();
 	}
 
 	/**
@@ -35,8 +62,8 @@ public class LabelEditorNodeFactory<T extends RealType<T> & NativeType<T>, L ext
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public NodeView<LabelEditorNodeModel<T>> createNodeView(final int i,
-			final LabelEditorNodeModel<T> nodeModel) {
+	public NodeView<LabelingEditorNodeModel<T>> createNodeView(final int i,
+			final LabelingEditorNodeModel<T> nodeModel) {
 		return new TableCellViewNodeView(nodeModel);
 	}
 
@@ -45,7 +72,7 @@ public class LabelEditorNodeFactory<T extends RealType<T> & NativeType<T>, L ext
 	 */
 	@Override
 	public NodeDialogPane createNodeDialogPane() {
-		return new LabelEditorNodeDialog<T, L>();
+		return new LabelingEditorNodeDialog<T, L>();
 	}
 
 	/**
